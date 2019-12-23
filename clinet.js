6f54ca6e2e858edff5a019255c71ed924f65e040
Brad Barrows <bradbarrows@peachjar.com>
2019-12-23 02:16:40 -0800

    init

---
+++ clinet.js
@@ -0,0 +1,20 @@
+//Multicast Client receiving sent messages
+var PORT = 41848;
+var MCAST_ADDR = "230.185.192.108"; //same mcast address as Server
+var HOST = '192.168.1.9'; //this is your own IP
+var dgram = require('dgram');
+var client = dgram.createSocket('udp4');
+
+client.on('listening', function () {
+    var address = client.address();
+    console.log('UDP Client listening on ' + address.address + ":" + address.port);
+    client.setBroadcast(true)
+    client.setMulticastTTL(128);
+    client.addMembership(MCAST_ADDR);
+});
+
+client.on('message', function (message, remote) {
+    console.log('MCast Msg: From: ' + remote.address + ':' + remote.port +' - ' + message);
+});
+
+client.bind(PORT, HOST);
